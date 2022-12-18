347b36a656ce4f549df8d6103e52ad900963c8c9
Alpfreda <alpfreda@gmail.com>
2022-12-18 18:59:40 +0300

    Add icons

---
+++ src/components/error/404.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Error404 = () => {
+  return <section className='introduce-summary'>
+    404
+  </section>
+}
+
+export { Error404 }
---
+++ src/pages/layout/navbar/logo.tsx
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const Logo = () => <div className='logo'>Mehdi Akbarzadeh</div>
+
+export { Logo }
