2b1c8a3f3fba8079e071629656eff59f8dfa65b3
Alpfreda <alpfreda@gmail.com>
2022-12-30 15:48:53 +0300

    Add loading to app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,20 +1,46 @@
 import '../assets/main.scss'
 import { Layout } from './layout'
+import { Provider } from 'react-redux'
+import { store } from '../app/store'
+import Router from "next/router"
+import { useState, useEffect } from 'react'
+// import Head from "next/head"
+// import Router from "next/router"
 
-// export default function App({ Component, pageProps }) {
-//   return <Component {...pageProps} />
+// Router.onRouteChangeStart = url => {
+//   NProgress.start()
 // }
 
-// import React from 'react'
-import { Provider } from 'react-redux'
-// import App from '../app'
-import { store } from '../app/store'
-// import '../assets/main.scss'
+// Router.onRouteChangeComplete = () => NProgress.done()
+
+// Router.onRouteChangeError = () => NProgress.done()
 
 const App = ({ Component, pageProps }) => {
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const start = () => {
+      console.log("start")
+      setLoading(true)
+    }
+    const end = () => {
+      console.log("finished")
+      setLoading(false)
+    }
+    Router.events.on("routeChangeStart", start)
+    Router.events.on("routeChangeComplete", end)
+    Router.events.on("routeChangeError", end)
+    return () => {
+      Router.events.off("routeChangeStart", start)
+      Router.events.off("routeChangeComplete", end)
+      Router.events.off("routeChangeError", end)
+    }
+  }, [])
+
   return <Provider store={store}>
     <Layout>
-      <Component {...pageProps} />
+      {loading ? <div>Loading...</div>:<Component {...pageProps} />}
+      
     </Layout>
   </Provider>
 }
